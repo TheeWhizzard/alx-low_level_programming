cf5585d68116e9330e10f5c594bb2af7c637e1b1
TheeWhizzard <boazleonard@gmail.com>
2023-06-16 07:20:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<unistd.h>
 /**
- * main - a program that prnts a line to the standard error
- * Return: 1 (success)
- */
-int main(void)
+* main - a program that prnts a line to the standard error
+* Return: 1 (success)
+*/
+intmain(void)
 {
-      write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return (1);
+write (2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59 );
+return (1);
 }
