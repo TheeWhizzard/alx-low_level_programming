33b2cb06f3b78e1d3d0c9c1f227d5a06da0e2760
TheeWhizzard <boazleonard@gmail.com>
2023-06-16 07:25:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
 * main - a program that prnts a line to the standard error
 * Return: 1 (success)
 */
-intmain(void)
+int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-return (1);
+       write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59); 
+       return (1);
 }
