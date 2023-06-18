1323871b06a832d8bf5f48842af9938f1c3eb537
TheeWhizzard <boazleonard@gmail.com>
2023-06-17 23:40:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - a program that prints a line to the standard error
+* main - a program that prints standard error line
 * Return: 1 (success)
 */
 int main(void)
 {
-       write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+       write (2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
        return (1);
 }
