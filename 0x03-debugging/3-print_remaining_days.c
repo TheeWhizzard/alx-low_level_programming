3e94575af0f1e7fc7d5e22e47a41a1ab0678796c
TheeWhizzard <boazleonard@gmail.com>
2023-06-22 02:28:17 -0700

    debbuging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,9 @@
 */
 voidprint_remaining_days(int month, int day, int year)
 {
-if((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
-if(month > 2 && day >= 60)
+if (month > 2 && day >= 60)
 {
 day++;
 }
@@ -21,7 +21,7 @@ printf("Remainingdays: %d\n", 366 - day);
 }
 else
 {
-if(month == 2 && day == 60)
+if (month == 2 && day == 60)
 {
 printf("Invaliddate: %02d/%02d/%04d\n", month, day - 31, year);
 }
